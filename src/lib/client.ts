7b07a45a7ed1c51f597c13ed52025d76b03ff882
oguz <oguz.oeztuerk@outlook.de>
2025-12-13 23:29:06 +0100

    feat: Add NEXT_PUBLIC_VERCEL_URL as an additional fallback for the client URL.

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,7 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 export const client = treaty<App>(
-  process.env.NEXT_PUBLIC_APP_URL ?? 'localhost:3000'
+  process.env.NEXT_PUBLIC_APP_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    'localhost:3000'
 ).api
